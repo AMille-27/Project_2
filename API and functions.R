d1870fccf70b52e3dde4906fa127f2329ec65ff6
AMille-27 <Amille27@ncsu.edu>
2025-07-07 18:06:05 -0400

    edited Readme overview and updated summarizing data with 2 charts.

--- API and functions.R
+++ API and functions.R
@@ -1,9 +1,12 @@
 
-#API Vegan Meals Query 
+# Find Data You Are Interested In
 
 library(jsonlite)
 library(tidyverse)
+library(purrr)
 library(httr)
+#API Vegan Meals Query 
+
 Vegan_meals_query <- "https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegan"
 Vegan_meals_q_response <- GET(Vegan_meals_query)
 Vegan_meals_q_r_parsed <-fromJSON(rawToChar(Vegan_meals_q_response$content))
@@ -11,7 +14,7 @@ Vegan_meals_q_r_p_tibble <-as_tibble(Vegan_meals_q_r_parsed)
 
 #the above is too small in my opinion
 
-#API for meal Category..Vegan and vegetarian = plant based vs contains meat = other
+## API for meal Category..Vegan and vegetarian = plant based vs contains meat = other
 all_dietary_query <-"https://www.themealdb.com/api/json/v1/1/categories.php"
 all_dietary_q_responses <- GET(all_dietary_query)
 all_dietary_q_r_parsed <- fromJSON(rawToChar(all_dietary_q_responses$content))
@@ -21,38 +24,23 @@ all_diet_type_grouped <- all_dietary_q_r_p_tibble$categories |>
     diet_type = if_else(strCategory %in% c("Vegan", "Vegetarian"), "Plant-Based","Other")
       )
 
-#API and function for meals by region
+## API and function for meals by region
 get_meals_by_region <- function(region_name){
-  url2 <- paste0("https://www.themealdb.com/api/json/v1/1/filter.php?a=", region_name)
+  url2 <- paste("https://www.themealdb.com/api/json/v1/1/filter.php?a=", region_name, sep = "")
   response <- GET(url2)
   parsed <-fromJSON(rawToChar(response$content))
   as_tibble(parsed$meals)
 }
 
-#API and function for Youtube link present or not and region
-Youtube_or_not <- function(user_input, has_video = TRUE){
-  url3 <- paste0("https://www.themealdb.com/api/json/v1/1/filter.php?a=", user_input)
-  response <- GET(url3)
-  parsed <- fromJSON(rawToChar(response$content))
-  meals_area <- as_tibble(parsed$meals)
-  if (has_video) {
-    meals_area <- meals_area %>% filter(strYoutube !="")
-  } else{
-    meals_area <- meals_area %>% filter(strYoutube == "")
-  }
-  return(meals_area)
-}
-#above not working as intended ughhhhhh
-
 #going to another query
-#Meals by category
+## Meals by category
 all_dietary_query <- "https://www.themealdb.com/api/json/v1/1/categories.php"
 all_dietary_response <- GET(all_dietary_query)
 all_dietary_parsed <- fromJSON(rawToChar(all_dietary_response$content))
 all_categories <- as_tibble(all_dietary_parsed$categories)
 
 get_meals_by_category <-function(category){
-  url4 <-paste0("https://www.themealdb.com/api/json/v1/1/filter.php?c=", category)
+  url4 <-paste("https://www.themealdb.com/api/json/v1/1/filter.php?c=", category,sep = "")
   response <- GET(url4)
   parsed <- fromJSON(rawToChar(response$content))
   all_df <-as_tibble(parsed$meals)
@@ -60,14 +48,15 @@ get_meals_by_category <-function(category){
   return(all_df)
 }
 
-#meals by Number of ingredients
+## meals by Number of ingredients
+
 all_categories_cv <- c("Beef","Chicken","Dessert","Lamb","Miscellaneous",
       "Pasta" ,"Pork","Seafood" ,"Side","Starter", "Vegan","Vegetarian",
       "Breakfast","Goat")
 all_meals_df <- purrr::map_dfr(all_categories_cv, get_meals_by_category)
 
 get_meals_by_id <-function(id){
-  url5 <-paste0("https://www.themealdb.com/api/json/v1/1/lookup.php?i=", id)
+  url5 <-paste("https://www.themealdb.com/api/json/v1/1/lookup.php?i=", id, sep ="")
   response <- GET(url5)
   parsed <- fromJSON(rawToChar(response$content))
   all_id <- as_tibble(parsed$meals)
@@ -94,3 +83,53 @@ full_all_meals <- full_all_meals |>
   left_join(count_all_meals_ingredients, by = "idMeal")
  
   
+## API and function for Youtube link present 
+Youtube <- function(data, has_video) {
+  if (has_video) {
+    data |> 
+      dplyr::filter(!is.na(.data$strYoutube) & nchar(.data$strYoutube) > 0)
+  } else {
+    data |>
+      dplyr::filter(is.na(.data$strYoutube) | nchar(.data$strYoutube) == 0)
+  }
+}
+#Do I need to go back to add a yes or no column to the table or make a new tibble??
+
+## Recipe Meal Name length 
+full_all_meals <- full_all_meals |>
+  mutate(meal_name_length = nchar(strMeal))
+
+## Recipe Instruction Length
+full_all_meals <- full_all_meals |>
+  mutate(instruction_length = nchar(strInstructions))
+
+## adding everything to one tibble to make summarize easier
+full_all_meals <-full_all_meals |> 
+  mutate(diet_type = if_else(strCategory %in% c("Vegan", "Vegetarian"), "Plant-Based","Other"),
+         has_youtube = if_else(!is.na(strYoutube) & nchar(strYoutube) > 0, "Yes", "No") )
+
+# Know How to Summarize the Data
+
+## Video Availability by Area  
+full_all_meals |>
+  count(strArea, has_youtube) |>
+  ggplot(aes(fill= has_youtube, y= n, x = strArea))+
+  geom_bar(position = "stack", stat= "identity") +
+  coord_flip() +
+  geom_text(aes(label =n))+
+  labs(title = "Video Availability by Area", y = "Number of meals", x = "Region")
+  
+## Plant Based or other by region contingency table
+ full_all_meals |>
+   count(diet_type, strArea) |>
+   ggplot(aes(fill = diet_type, y = n, x = strArea)) +
+   geom_bar(position = "stack", stat = "identity")+
+   coord_flip() +
+   geom_text(aes(label =n))+
+   labs(title = "Plant-Based Vs Other by Area", y = "Number of meals", x = "Region")
+
+ ## Contingency table "Number of ingredients" and Youtube videos 
+ # need to categorize complexity based on number of ingredients. 
+ 
+ 
+
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Project_2
 ## Brief description of the app and its purpose.
-This app uses TheMealDB API. This will be used to search through meals and recipe ideas. Users will be able to search using dietary preferences such as vegan and vegetarian. 
+This app uses TheMealDB API. This will be used to search through meals and recipe ideas. Users will be able to search using dietary preferences such as vegan and vegetarian, 
+Area, number of ingredients, number of characters from the instructions and whether there's a youtube video.
 ## A list of packages needed to run the app.
 Users will need the following packages:
 Tidyverse
